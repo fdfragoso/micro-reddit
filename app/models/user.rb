56adbdfc2fe50c20a53216f437223116b64d0755
keddo <adu.kedir.abdurahman@gmail.com>
2020-09-15 16:07:05 +0300

    Add commenting

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ApplicationRecord
   has_many :comments
   has_many :posts
 
-  validates :user_name, uniqueness: true, length: { in: 5..20 }
+  validates :user_name, uniqueness: true, length: { in: 3..20 }
 end
