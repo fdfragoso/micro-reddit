c3e71d93658ea562c321de129c5816e801e80887
keddo <adu.kedir.abdurahman@gmail.com>
2020-09-16 09:13:48 +0300

    Add comment author and body validation

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,8 @@ class Comment < ApplicationRecord
   belongs_to :post
   belongs_to :user
 
+  validates :author_name, presence: true
+  validates :body, length: {in: 15..500}
   validates :user_id, presence: true
   validates :post_id, presence: true
 end
