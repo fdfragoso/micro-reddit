18c762eea8e6bfeaf80dbbdf5f230e9a0560a1d0
keddo <adu.kedir.abdurahman@gmail.com>
2020-09-16 09:22:59 +0300

    Add email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@ class User < ApplicationRecord
   has_many :posts
 
   validates :user_name, uniqueness: true, length: { in: 3..20 }
+  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
 end
